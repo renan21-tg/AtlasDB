3509f48e07a2b862c21a9dbf713734b53f5cfea2
renan21-tg <rnantg21@gmail.com>
2025-11-10 10:49:44 -0300

    feat: Adicionando banner para home

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -6,20 +6,20 @@ function Navbar() {
             <header className="w-full h-20 border border-b-gray-300 flex justify-center items-center">
                 <div className="w-9/10 h-full flex justify-between">
                     <div className="flex items-center">
-                        <GlobeEuropeAfricaIcon className="w-9 h-9  fill-orange-500 mr-2" />
+                        <GlobeEuropeAfricaIcon className="w-9 h-9  fill-emerald-700 mr-2" />
                         <p className="text-xl font-bold">AtlasDB</p>
                     </div>
 
                     <nav className="w-60 flex items-center">
                         <ul className="w-full list-none flex justify-around">
-                            <div className="hover:scale-110 hover:text-orange-500 duration-200 ease-in-out">
-                                <li><a href="#" className=" ">Continentes</a></li>
+                            <div className="mr-4 hover:scale-110 hover:text-emerald-900 duration-200 ease-in-out">
+                                <li><a href="#">Continentes</a></li>
                             </div>
-                            <div className="hover:scale-110 hover:text-orange-500 duration-200 ease-in-out">
-                                <li><a href="#" className="hover:text-orange-500 ease-linear">Países</a></li>
+                            <div className="mr-4 hover:scale-110 hover:text-emerald-900 duration-200 ease-in-out">
+                                <li><a href="#">Países</a></li>
                             </div>
-                            <div className="hover:scale-110 hover:text-orange-500 duration-200 ease-in-out">
-                                <li><a href="#" className="hover:text-orange-500 ease-linear" >Cidades</a></li>
+                            <div className="hover:scale-110 hover:text-emerald-900 duration-200 ease-in-out">
+                                <li><a href="#">Cidades</a></li>
                             </div>
                         </ul>
                     </nav>
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import Navbar from "../components/navbar"
+import Navbar from "../components/Navbar"
 
 function Home() {
     return (
         <>
             <Navbar />
+            <section>
+                <div className="h-[75vh] w-full bg-cover flex justify-center items-center bg-[url(https://images.unsplash.com/photo-1661705969607-cde73828023d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFwZWwlMjBkZSUyMHBhcmVkZSUyMHRlcnJhfGVufDB8fDB8fHww&fm=jpg&q=60&w=3000)]"> 
+                    <div className="flex flex-col items-center">
+                        <div className="text-8xl text-shadow-lg/40 text-shadow-black text-teal-50 font-semibold italic mb-3">Atlas DB</div>
+                        <p className="mb-16 text-xl text-emerald-600 text-shadow-emerald-900 text-shadow-md">Gerencie e explore informações geográficas com facilidade.</p>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
